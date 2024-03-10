6b73258bd149e527b47ad9d12d2d3185501370a0
UzairKhalil <UzairKhalil@users.noreply.github.com>
2024-03-10 19:05:38 +0000

    Fix styling

--- src/AmazonReportsServiceProvider.php
+++ src/AmazonReportsServiceProvider.php
@@ -2,19 +2,10 @@
 
 namespace Techxa\AmazonReports;
 
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
-use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Techxa\AmazonReports\Commands\AmazonReportsCommand;
-use Techxa\AmazonReports\Testing\TestsAmazonReports;
 
 class AmazonReportsServiceProvider extends PackageServiceProvider
 {
@@ -30,7 +21,7 @@ class AmazonReportsServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         FilamentAsset::register([
-            Css::make('headings', __DIR__ . '/../resources/dist/amazonreports.css')->loadedOnRequest(),
+            Css::make('headings', __DIR__.'/../resources/dist/amazonreports.css')->loadedOnRequest(),
         ], 'techxa/laravel-amazon-reports');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,7 +27,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Techxa\\AmazonReports\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'Techxa\\AmazonReports\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
