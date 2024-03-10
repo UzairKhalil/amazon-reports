794409b0364817cc971682fe505db9845746cecc
uzairkhalil <uzairm2607@gmail.com>
2024-03-11 00:05:04 +0500

    install dependencies

--- src/AmazonReportsServiceProvider.php
+++ src/AmazonReportsServiceProvider.php
@@ -24,131 +24,13 @@ class AmazonReportsServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('techxa/laravel-amazon-reports');
-            });
-
-        $configFileName = $package->shortName();
-
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
-    }
-
-    public function packageRegistered(): void
-    {
+        $package->name(static::$name)->hasViews();
     }
 
     public function packageBooted(): void
     {
-        // Asset Registration
-        FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
-        );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/laravel-amazon-reports/{$file->getFilename()}"),
-                ], 'laravel-amazon-reports-stubs');
-            }
-        }
-
-        // Testing
-        Testable::mixin(new TestsAmazonReports());
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'techxa/laravel-amazon-reports';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('laravel-amazon-reports', __DIR__ . '/../resources/dist/components/laravel-amazon-reports.js'),
-            Css::make('laravel-amazon-reports-styles', __DIR__ . '/../resources/dist/laravel-amazon-reports.css'),
-            Js::make('laravel-amazon-reports-scripts', __DIR__ . '/../resources/dist/laravel-amazon-reports.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            AmazonReportsCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_laravel-amazon-reports_table',
-        ];
+        FilamentAsset::register([
+            Css::make('headings', __DIR__ . '/../resources/dist/amazonreports.css')->loadedOnRequest(),
+        ], 'techxa/laravel-amazon-reports');
     }
 }
